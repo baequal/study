1668403678d1334ac56597ce693e655396308d9d
baequal <114137422+baequal@users.noreply.github.com>
2022-09-22 21:02:41 +0900

    Create 나머지가 1이 되는 수 찾기.cs

---
+++ 나머지가 1이 되는 수 찾기.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codingstudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 10;
+
+            Program p = new Program();
+
+            int output = p.solution(input);
+            Console.WriteLine(output);
+        }
+        //input 10 output 3
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 1)
+                {
+                    answer = i;
+                    break;
+                }
+
+            }
+
+            return answer;
+        }
+    }
+}
