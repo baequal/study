d9bdb01e22ec10c646d9dba89a7ff9d0a382f6cf
baequal <114137422+baequal@users.noreply.github.com>
2022-09-22 20:30:54 +0900

    Create 약수의 합.cs

---
+++ 약수의 합.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codingstudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 12;
+
+            Program p = new Program();
+
+            int output = p.solution(input);
+            Console.WriteLine(output);
+        }
+        //in 12 out	28
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    answer += i;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
