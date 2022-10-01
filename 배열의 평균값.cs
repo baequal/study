9d7924c8a3fb8855b378fde79f54124219d079be
baequal <114137422+baequal@users.noreply.github.com>
2022-10-01 13:06:42 +0900

    Create 배열의 평균값.cs

---
+++ 배열의 평균값.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codingstudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            
+            Program p = new Program();
+
+            double output = p.solution(input1);
+            Console.WriteLine(output);
+        }
+        //numbers	                           result
+        //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]	 5.5
+        public double solution(int[] numbers)
+        {
+            double answer = 0;
+
+            foreach (int i in numbers)
+            {
+                answer += i;
+            }
+            answer = answer / numbers.Length;
+
+            return answer;
+        }
+    }
+}
